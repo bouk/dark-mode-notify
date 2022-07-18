b452fd4d7b6ff951c4b3683e2c5dbf52012b8580
Jens-Uwe Mager <jum@anubis.han.de>
2022-07-18 13:39:08 +0200

    Add Makefile and swift pm declarations for building.
    This prepares the program to be submitted to homebrew.

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "dark-mode-notify",
+    dependencies: [
+    ],
+    targets: [
+    	.target(
+		name: "dark-mode-notify",
+		path: "."
+	)
+    ]
+)
+
